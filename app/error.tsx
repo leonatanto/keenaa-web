3030a28255d06f00d313939cea91beaa0c72ac7e
Leon Natanto <103975190+leonatanto@users.noreply.github.com>
2025-06-12 23:11:33 +0700

    chore: fix bg

--- app/error.tsx
+++ app/error.tsx
@@ -15,7 +15,7 @@ export default function Error({
   }, [error]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 text-center">
         <div>
           <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-white">
